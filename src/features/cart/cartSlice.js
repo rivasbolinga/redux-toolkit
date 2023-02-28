a4628d64b17539b116d11cf82eb88dcdfa942d9f
Andrea <andrearivas4cinfor@gmail.com>
2023-02-28 12:20:06 +0100

    remove, clear, decrease and increase items

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -11,13 +11,25 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    clearCart:(state)=> {
+    clearCart: (state) => {
       state.cartItems = [];
-    }
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
 
   }
 })
 //
-export const { clearCart } = cartSlice.actions
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions
 
 export default cartSlice.reducer;
