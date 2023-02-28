6398f54609fff4d5eb4626e1734d306ad9fcfc64
Andrea <andrearivas4cinfor@gmail.com>
2023-02-28 11:57:35 +0100

    Clar items function

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,16 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit'
-import cartItems from '../../cartItems'
+import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems';
 const initialState = {
   cartItems : cartItems,
   amount: 4,
   total: 0,
   isLoading: true
 }
+//-- we can mutate the state thanks immer. when we clear all the items, we mutate the items array to an empty array
 const cartSlice = createSlice({
   name: 'cart',
-  initialState
-})
+  initialState,
+  reducers: {
+    clearCart:(state)=> {
+      state.cartItems = [];
+    }
 
-console.log(cartSlice)
-  
+  }
+})
+//
+export const { clearCart } = cartSlice.actions
+
 export default cartSlice.reducer;
